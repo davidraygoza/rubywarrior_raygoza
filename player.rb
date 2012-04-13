60b3fe53658d929d1a127feb98bdefcbd1c5f707
David Israel Raygoza Ramírez <david.raygoza.ramirez@gmail.com>
2012-04-13 17:59:18 -0500

    Nivel 7

--- player.rb
+++ player.rb
@@ -1,10 +1,15 @@
+#Practice by: David Raygoza Ramirez
+#Date: April 13 2012
+#Level 7 Code
 class Player
 @health_warrior=nil
 
   def play_turn(warrior)
 
     @health_warrior=warrior.health unless @health_warrior
-     
+    if warrior.feel.wall? then 
+      warrior.pivot!
+    else
     if warrior.feel.empty? then
       if warrior.health<@health_warrior then
         if warrior.health<8 then
@@ -29,5 +34,6 @@ class Player
         
     end
       @health_warrior = warrior.health
+  end
   end
 end
